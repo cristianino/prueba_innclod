534e8be753fa39d433618a584e1db217c455ae4a
TecnoVulario <cristiandavid113@gmail.com>
2023-11-04 21:55:16 -0500

    Feature/add documentation models (#3)
    * chore: execute php artisan make:model ProProceso --all
    
    * chore: execute php artisan make:model TipTipoDoc --all && php artisan make:model DocDocumento --all
    
    * fix: quite drop foring keys to down doc_document pethod

---
+++ app/Http/Controllers/DocDocumentoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreDocDocumentoRequest;
+use App\Http\Requests\UpdateDocDocumentoRequest;
+use App\Models\DocDocumento;
+
+class DocDocumentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDocDocumentoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DocDocumento $docDocumento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DocDocumento $docDocumento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDocDocumentoRequest $request, DocDocumento $docDocumento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DocDocumento $docDocumento)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_11_05_014645_create_doc_documentos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('DOC_DOCUMENTO', function (Blueprint $table) {
+            $table->increments('DOC_ID');
+            $table->string('DOC_NOMBRE', 50);
+            $table->unsignedInteger('DOC_ID_TIPO');
+            $table->unsignedInteger('DOC_ID_PROCESO');
+            $table->timestamps();
+
+            $table->foreign('DOC_ID_TIPO')->references('TIP_ID')->on('TIP_TIPO_DOC');
+            $table->foreign('DOC_ID_PROCESO')->references('PRO_ID')->on('PRO_PROCESO');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('DOC_DOCUMENTO');
+    }
+};
