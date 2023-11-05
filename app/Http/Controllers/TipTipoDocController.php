e64081ebeeafe318bc08b5e7c42cced7e735a460
TecnoVulario <cristiandavid113@gmail.com>
2023-11-04 21:54:23 -0500

    Feature/add documentation models (#2)
    * Feature/add auth (#1)
    
    * chore: install breeze and add documentation
    
    * chore: change reamde
    
    * chore: execute php artisan make:model ProProceso --all
    
    * chore: execute php artisan make:model TipTipoDoc --all && php artisan make:model DocDocumento --all
    
    * fix: quite drop foring keys to down doc_document pethod

---
+++ app/Http/Controllers/TipTipoDocController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTipTipoDocRequest;
+use App\Http\Requests\UpdateTipTipoDocRequest;
+use App\Models\TipTipoDoc;
+
+class TipTipoDocController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTipTipoDocRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TipTipoDoc $tipTipoDoc)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TipTipoDoc $tipTipoDoc)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTipTipoDocRequest $request, TipTipoDoc $tipTipoDoc)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TipTipoDoc $tipTipoDoc)
+    {
+        //
+    }
+}
