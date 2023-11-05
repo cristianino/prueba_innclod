06f73f0a7db3b7057ca6bd55c94e3d25cbbdf784
Cristian Niño <cristiandavid113@gmail.com>
2023-11-05 01:26:41 -0500

    cohre: add relationshis and personal variables to models

--- app/Models/TipTipoDoc.php
+++ app/Models/TipTipoDoc.php
@@ -5,7 +5,30 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\DocDocumento;
+
 class TipTipoDoc extends Model
 {
     use HasFactory;
+
+    protected $table = 'TIP_TIPO_DOC';
+    protected $primaryKey = 'TIP_ID';
+
+    public $incrementing = true;
+
+
+    protected $fillable = ['TIP_PREFIJO', 'TIP_NOMBRE'];
+
+    protected $guarded = ['TIP_ID'];
+
+    protected $casts = [
+        'TIP_PREFIJO' => 'string',
+        'TIP_NOMBRE' => 'string',
+    ];
+
+
+    public function documentos()
+    {
+        return $this->hasMany(DocDocumento::class, 'DOC_ID_TIPO', 'TIP_ID');
+    }
 }
