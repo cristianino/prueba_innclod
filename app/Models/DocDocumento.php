e16b52a21a3ce2607d3423c2c4067c890cb0b328
TecnoVulario <cristiandavid113@gmail.com>
2023-11-06 13:37:23 -0500

    Feature/add relationships (#5)
    * chore: add colunms to doc_documtento
    
    * cohre: add relationshis and personal variables to models

--- app/Models/DocDocumento.php
+++ app/Models/DocDocumento.php
@@ -5,7 +5,41 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\TipTipoDoc;
+use App\Models\ProProceso;
+
 class DocDocumento extends Model
 {
     use HasFactory;
+
+    protected $table = 'DOC_DOCUMENTO';
+
+    protected $primaryKey = 'DOC_ID';
+
+    public $incrementing = true;
+
+
+    protected $fillable = [
+        'DOC_NOMBRE',
+        'DOC_CODIGO',
+        'DOC_CONTENIDO',
+        'DOC_ID_TIPO',
+        'DOC_ID_PROCESO',
+    ];
+
+    public function tipoDoc()
+    {
+        return $this->belongsTo(TipTipoDoc::class, 'DOC_ID_TIPO', 'TIP_ID');
+    }
+
+    public function proceso()
+    {
+        return $this->belongsTo(ProProceso::class, 'DOC_ID_PROCESO', 'PRO_ID');
+    }
+
+    protected $casts = [
+        'DOC_NOMBRE' => 'string',
+        'DOC_CODIGO' => 'integer',
+        'DOC_CONTENIDO' => 'string',
+    ];
 }
