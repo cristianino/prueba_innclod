ad9913c789ee12f2b1184c2c9b94c77c62a431e6
Cristian Niño <cristiandavid113@gmail.com>
2023-11-06 23:07:13 -0500

    chore: quite inessesary methods

--- app/Http/Controllers/TipTipoDocController.php
+++ app/Http/Controllers/TipTipoDocController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreTipTipoDocRequest;
-use App\Http\Requests\UpdateTipTipoDocRequest;
 use App\Models\TipTipoDoc;
 
 class TipTipoDocController extends Controller
@@ -17,49 +15,9 @@ class TipTipoDocController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreTipTipoDocRequest $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(TipTipoDoc $tipTipoDoc)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(TipTipoDoc $tipTipoDoc)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateTipTipoDocRequest $request, TipTipoDoc $tipTipoDoc)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(TipTipoDoc $tipTipoDoc)
     {
         //
     }
