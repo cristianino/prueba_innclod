b705f9a8c976d162c7ea3a7b5e5357d81f1077b3
Cristian Niño <cristiandavid113@gmail.com>
2023-11-06 22:01:27 -0500

    chore: add tip tipo seeder

--- database/seeders/TipTipoDocSeeder.php
+++ database/seeders/TipTipoDocSeeder.php
@@ -12,6 +12,16 @@ class TipTipoDocSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $tiposDocumentos = [
+            ['TIP_PREFIJO' => 'INGC', 'TIP_NOMBRE' => 'Plan de Ingeniería Civil'],
+            ['TIP_PREFIJO' => 'INGE', 'TIP_NOMBRE' => 'Manual de Ingeniería Eléctrica'],
+            ['TIP_PREFIJO' => 'INGM', 'TIP_NOMBRE' => 'Informe de Ingeniería Mecánica'],
+            ['TIP_PREFIJO' => 'INGS', 'TIP_NOMBRE' => 'Protocolo de Ingeniería de Software'],
+            ['TIP_PREFIJO' => 'INGQ', 'TIP_NOMBRE' => 'Proyecto de Ingeniería Química'],
+        ];
+        
+        foreach ($tiposDocumentos as $tipo) {
+            \App\Models\TipTipoDoc::factory()->create($tipo);
+        }
     }
 }
