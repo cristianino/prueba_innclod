648e959bf7b6e3a3aa00504dd92e0f85e77baf3b
Cristian Niño <cristiandavid113@gmail.com>
2023-11-07 21:43:34 -0500

    chore: Set documnto rute to defult

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -29,8 +29,8 @@ const showingNavigationDropdown = ref(false);
 
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Dashboard
+                                <NavLink :href="route('documento.index')" :active="route().current('documento.index')">
+                                    Documentos
                                 </NavLink>
                             </div>
                         </div>
